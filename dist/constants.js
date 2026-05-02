d3b066092cd43daf7e32bf61c6b0557daa35d660
Daniel <danielwalkemdowntohell@gmail.com>
2026-05-02 04:36:54 +0200

    idk

--- dist/constants.js
+++ dist/constants.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SOURCE_URL = exports.TMDB_META_URL = void 0;
-exports.TMDB_META_URL = "https://94c8cb9f702d-tmdb-addon.baby-beamup.club/meta";
+exports.TMDB_META_URL = "https://v3-cinemeta.strem.io/meta";
 exports.SOURCE_URL = "https://vsembed.ru/embed";
