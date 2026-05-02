0521b8676fa004a2b25e45db794555390efdf16e
Daniel <danielwalkemdowntohell@gmail.com>
2026-05-02 04:34:51 +0200

    fixed to a db that dont need api key

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const TMDB_META_URL = "https://94c8cb9f702d-tmdb-addon.baby-beamup.club/meta";
+export const TMDB_META_URL = "https://v3-cinemeta.strem.io/meta";
 export const SOURCE_URL = "https://vsembed.ru/embed";
